f1256703a99147bd685183eb70736f6705d40751
Tamunonengiyeofori <tkennwariso@gmail.com>
2025-02-08 21:15:24 +0000

    Modified config.py file, extracted admission data from PDF file, added openAI secret Key, generated question and answer pairs and stored txt file in raw data folder

---
+++ config/settings.py
@@ -0,0 +1,21 @@
+# from openai import OpenAI
+# from dotenv import load_dotenv()
+# client = OpenAI()
+
+# # load all the environment variables from the .env file
+
+
+
+# completion = client.chat.completions.create(
+#     model="gpt-4o",
+#     messages=[
+#         {"role": "developer", "content": "You are a helpful assistant."},
+#         {
+#             "role": "user",
+#             "content": "Write a haiku about recursion in programming."
+#         }
+#     ]
+# )
+
+# print(completion.choices[0].message)
+
